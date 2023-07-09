427293e1aec0b334dc022d8c4a79f2f72d95fa46
DrDuck2 <118298726+DrDuck2@users.noreply.github.com>
2023-07-09 16:09:49 +0200

    Delete ServerSelection directory

--- ServerSelection/src/main/java/org/example/clientlibrary/window/IHandle.java
+++
@@ -1,4 +0,0 @@
-package org.example.clientlibrary.window;
-
-public interface IHandle {
-}
--- ServerSelection/src/main/java/org/example/clientlibrary/window/IScreen.java
+++
@@ -1,7 +0,0 @@
-package org.example.clientlibrary.window;
-
-public interface IScreen {
-
-    void init();
-    void display();
-}
